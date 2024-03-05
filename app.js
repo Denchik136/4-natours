3e1574ce650e8ff9a8f6140498a8bc2f01f91af0
Denchik <pdv@sinergium.ru>
2024-03-05 14:26:51 +0300

    patch

--- app.js
+++ app.js
@@ -19,6 +19,28 @@ app.get('/api/v1/tours', (req, res) => {
     })
 })
 
+app.get('/api/v1/tours/:id', (req, res) => {
+
+    const id = +req.params.id
+
+    const tour = tours.find(el => el.id === id)
+
+    if (!tour) {
+        return res.status(404).json({
+            status: "Fail",
+            message: "Invalid ID"
+        })
+    }
+
+
+    res.status(200).json({
+        status: "sucess",
+        data: {
+            tour
+        }
+    })
+})
+
 app.post('/api/v1/tours', (req, res) => {
     const newId = tours[tours.length - 1].id + 1
     const newTour = Object.assign({ id: newId }, req.body)
@@ -30,6 +52,23 @@ app.post('/api/v1/tours', (req, res) => {
                 tours: newTour
             }
         })
+    })
+})
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+
+    if (+req.params.id > tours.length) {
+        return res.status(404).json({
+            status: "Fail",
+            message: "Invalid ID"
+        })
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: '<Updated tour here>'
+        }
     })
 })
 
