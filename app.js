e854a0e7b78e3ad21d945bc38b6e14331a2851fb
Denchik <pdv@sinergium.ru>
2024-03-05 19:56:05 +0300

    req delete

--- app.js
+++ app.js
@@ -72,6 +72,20 @@ app.patch('/api/v1/tours/:id', (req, res) => {
     })
 })
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+
+    if (+req.params.id > tours.length) {
+        return res.status(404).json({
+            status: "Fail",
+            message: "Invalid ID"
+        })
+    }
+
+    res.status(204).json({
+        status: 'success',
+        data: null
+    })
+})
 
 const port = 3000
 
